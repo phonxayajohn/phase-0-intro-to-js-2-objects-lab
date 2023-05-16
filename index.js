c64b3a6d1ffb7a30f1b0d005f30aeec116334510
John Phonxaya <phonxayajohn@gmail.com>
2023-05-16 01:47:04 -0700

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "John",
+    streetAddress: "42 Wallaby Way"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updateEmployee = {
+        ...employee,
+        [key]: value
+    };
+    return updateEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const removeEmployee = {
+        ...employee
+    }
+
+    delete removeEmployee[key];
+    return removeEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
